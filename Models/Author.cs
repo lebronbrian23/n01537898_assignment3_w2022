98d823ba31a510cdd5f185c7d7799468166552b5
lebronbrian23 <lebronbrian23@gmail.com>
2022-03-24 20:44:37 -0700

    Add project files.

---
+++ Models/Author.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace School.Models
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+
+        public string AuthorFName { get; set; }
+
+        public string AuthorLName { get; set; }
+
+        public string AuthorDate { get; set; }
+    }
+}
